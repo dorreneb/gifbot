0a08826ebccbe72e1e8975e3877cf76d9d84b063
Dorrene Brown <dobrown@microsoft.com>
2017-07-18 05:37:09 -0700

    Bot's done

--- gifbot/server.js
+++ gifbot/server.js
@@ -24,7 +24,7 @@ server.post('/api/messages', connector.listen());
 var bot = new builder.UniversalBot(connector, function (session) {
 
     //set up the service call to the cognitive services API
-    var options = {
+    var cogServicesOptions = {
         method : 'POST',
         json : true,
         url : 'https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/keyPhrases',
@@ -42,25 +42,59 @@ var bot = new builder.UniversalBot(connector, function (session) {
         }
     };
 
+    var giphyOptions = {
+        method: 'GET',
+        json: true,
+        url: 'https://api.giphy.com/v1/gifs/search?api_key=' + process.env.GIPHY_API_KEY + '&limit=1&offset=0&rating=G&lang=en',
+        
+    };
+
     var keyPhrases = {};
+    var searchTerm = "";
     var responseString = "Something went wrong :(";
-    request(options)
+    var gifCard = null;
+
+    request(cogServicesOptions)
         .then(function (parsedBody) { //get the key phrases from the message
             if (parsedBody.documents.length > 0) {
+                //extract keyphrases from body response
                 keyPhrases = parsedBody.documents[0].keyPhrases;
+
+                //assemble search term for giphy
+                for (var term in keyPhrases) {
+                    searchTerm += keyPhrases[term].replace(" ", "+") + "+";
+                }
+                giphyOptions.url += "&q=" + searchTerm.slice(0, -1);
+
+                //call giphy API
+                return request(giphyOptions);
             } else {
                 responseString = "No documents returned.";
             } 
         })
-        .then(function () { //if there are key phrases, call giphy to get the right gif
-            if (keyPhrases) {
-                responseString = JSON.stringify(keyPhrases);
-            }
-        })
+        .then(function (gifBody) {
+            //get url
+            var embedUrl = gifBody.data[0].images.original.url;
+            console.log("embed " + gifBody.data[0].images.original.url);
+
+            var card = new builder.HeroCard(session)
+                .title('How about this one?')
+                .text('Seems like it could help!')
+                .images([
+                    builder.CardImage.create(session, embedUrl)
+                ]);
+
+            gifCard = new builder.Message(session).addAttachment(card);
+        }) 
         .catch(function (err) {
             responseString = "ERROR - " + err;
         }).then(function () { //actually send the message back to the client
-            session.send(responseString);
+            if (gifCard) {
+                session.send(gifCard);
+            } else {
+                session.send(responseString);
+            }
+            
         });
 
     
